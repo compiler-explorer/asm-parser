05a8d691506fa8a81ddd02215ad16067906539bd
partouf <partouf@gmail.com>
2020-10-17 17:38:25 +0000

    no idea what im doign

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install python3 python3-pip
-sudo pip3 install conan setuptools
+sudo -H apt install python3 python3-pip
+sudo -H pip3 install conan setuptools
 
 conan profile update settings.compiler.libcxx=libstdc++11 default
