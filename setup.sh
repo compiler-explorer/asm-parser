d755c93e83f4a9b7586fe6b9fc37fc4629255524
partouf <partouf@gmail.com>
2020-10-17 18:00:56 +0000

    try to install gcc 8

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo -H apt install -y python3 python3-pip python3-setuptools
+sudo -H apt install -y python3 python3-pip python3-setuptools g++-8
 sudo -H pip3 install conan
 
 #conan profile update settings.compiler.libcxx=libstdc++11 default
