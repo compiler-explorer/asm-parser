4e56922a9d60c25bfec949d8d13f35ab56700169
partouf <partouf@gmail.com>
2020-10-18 13:20:59 +0000

    parse command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,82 @@
+#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <string_view>
 
 #include "objdump/parser.hpp"
 #include "types/filter.hpp"
 
-int main(int argc, char **argv)
+struct AsmParserConfiguration
+{
+    AsmParser::Filter filter{};
+    std::string filename;
+    bool useStdin{};
+    bool isConfigured{};
+};
+
+bool streq(const std::string_view a, const std::string_view b)
+{
+    return a.compare(b) == 0;
+}
+
+AsmParserConfiguration getConfigurationFromCommandline(const int argc, const char **argv)
+{
+    AsmParserConfiguration config{};
+    config.filter.compatmode = true;
+
+    for (auto i = 1; i < argc; i++)
+    {
+        if (streq(argv[i], "-binary"))
+            config.filter.binary = true;
+        else if (streq(argv[i], "-unused_labels"))
+            config.filter.unused_labels = true;
+        else if (streq(argv[i], "-directives"))
+            config.filter.directives = true;
+        else if (streq(argv[i], "-comment_only"))
+            config.filter.comment_only = true;
+        else if (streq(argv[i], "-whitespace"))
+            config.filter.whitespace = true;
+        else if (streq(argv[i], "-nocompat"))
+            config.filter.compatmode = false;
+        else if (streq(argv[i], "-stdin"))
+        {
+            config.useStdin = true;
+            config.isConfigured = true;
+        }
+        else
+        {
+            config.filename = argv[i];
+            config.isConfigured = true;
+        }
+    }
+
+    return config;
+}
+
+int main(int argc, const char **argv)
 {
     setlocale(LC_ALL, "en_US.utf8");
 
-    AsmParser::Filter filter;
-    filter.compatmode = true;
+    const auto config = getConfigurationFromCommandline(argc, argv);
 
-    if (argc > 1)
+    if (!config.isConfigured)
     {
-        std::string asmfile = argv[1];
+        std::cout << "Commandline arguments expected\n";
+        return 1;
+    }
 
+    if (!config.useStdin)
+    {
         std::fstream fs;
-        fs.open(asmfile, std::fstream::in);
+        fs.open(config.filename, std::fstream::in);
 
-        AsmParser::ObjDumpParser parser(filter);
+        AsmParser::ObjDumpParser parser(config.filter);
         parser.fromStream(fs);
         parser.outputJson(std::cout);
     }
     else
     {
-        AsmParser::ObjDumpParser parser(filter);
+        AsmParser::ObjDumpParser parser(config.filter);
         parser.fromStream(std::cin);
         parser.outputJson(std::cout);
     }
--- test.sh
+++ test.sh
@@ -12,11 +12,11 @@ if [ $? -ne 0 ]; then
 fi
 cd ..
 
-build/bin/asm-parser /opt/compiler-explorer/ce/test/filters-cases/bintest-1.asm > /opt/compiler-explorer/ce/test/filters-cases/bintest-1.asm.binary.directives.labels.comments.json
+build/bin/asm-parser -binary /opt/compiler-explorer/ce/test/filters-cases/bintest-1.asm > /opt/compiler-explorer/ce/test/filters-cases/bintest-1.asm.binary.directives.labels.comments.json
 /opt/compiler-explorer/node/bin/node prettyjson.js /opt/compiler-explorer/ce/test/filters-cases/bintest-1.asm.binary.directives.labels.comments.json
 
-build/bin/asm-parser /opt/compiler-explorer/ce/test/filters-cases/bintest-2.asm > /opt/compiler-explorer/ce/test/filters-cases/bintest-2.asm.binary.directives.labels.comments.json
+build/bin/asm-parser -binary /opt/compiler-explorer/ce/test/filters-cases/bintest-2.asm > /opt/compiler-explorer/ce/test/filters-cases/bintest-2.asm.binary.directives.labels.comments.json
 /opt/compiler-explorer/node/bin/node prettyjson.js /opt/compiler-explorer/ce/test/filters-cases/bintest-2.asm.binary.directives.labels.comments.json
 
-build/bin/asm-parser /opt/compiler-explorer/ce/test/filters-cases/bintest-unicode-1.asm > /opt/compiler-explorer/ce/test/filters-cases/bintest-unicode-1.asm.binary.directives.labels.comments.json
+build/bin/asm-parser -binary /opt/compiler-explorer/ce/test/filters-cases/bintest-unicode-1.asm > /opt/compiler-explorer/ce/test/filters-cases/bintest-unicode-1.asm.binary.directives.labels.comments.json
 /opt/compiler-explorer/node/bin/node prettyjson.js /opt/compiler-explorer/ce/test/filters-cases/bintest-unicode-1.asm.binary.directives.labels.comments.json
