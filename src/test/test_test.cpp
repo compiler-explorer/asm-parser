5cbcbed3053dcdd44df5da8fff32f32e97e7d9f0
partouf <partouf@gmail.com>
2020-10-20 19:02:28 +0000

    remove raw regex tests

--- src/test/test_test.cpp
+++ src/test/test_test.cpp
@@ -14,17 +14,6 @@ TEST_CASE("Test some regexes directly", "[regex]")
     CHECK(AsmParser::Regexes::dataDefn(R"(  .string "Hello, world!\n")"));
 
     CHECK(!AsmParser::Regexes::labelDef("  mov eax, eax"));
-
-    const auto fileMatch =
-    AsmParser::Regexes::fileFind(R"(        .file 1 "/opt/compiler-explorer/gcc-10.2.0/include/c++/10.2.0/bits/char_traits.h")");
-    CHECK(fileMatch);
-    REQUIRE(fileMatch.get<1>().to_view() == "1");
-    REQUIRE(fileMatch.get<2>().to_view() == "/opt/compiler-explorer/gcc-10.2.0/include/c++/10.2.0/bits/char_traits.h");
-
-    const auto sourceMatch = AsmParser::Regexes::sourceTag(R"(        .loc 1 351 7)");
-    CHECK(sourceMatch);
-    REQUIRE(sourceMatch.get<1>().to_view() == "1");
-    REQUIRE(sourceMatch.get<2>().to_view() == "351");
 }
 
 TEST_CASE("Test text assembly utilities", "[asm]")
