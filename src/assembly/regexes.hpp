71b89f310e6af559fb92f5395bfb26822589fd18
partouf <partouf@gmail.com>
2020-10-20 17:20:17 +0000

    changed regexes

--- src/assembly/regexes.hpp
+++ src/assembly/regexes.hpp
@@ -6,54 +6,45 @@
 namespace AsmParser
 {
 
-template <ctll::fixed_string RE>
-constexpr auto make_matcher()
-{
-    return [](const std::string_view text) {
-        return ctre::match<RE>(text);
-    };
-}
-
 struct Regexes
 {
-    static constexpr auto labelDef = make_matcher<R"re(^(?:.proc\s+)?([\w$.@]+):)re">();
-    static constexpr auto labelFindNonMips = make_matcher<R"re([.A-Z_a-z][\w$.]*)re">();
-    static constexpr auto labelFindMips = make_matcher<R"re([$.A-Z_a-z][\w$.]*)re">();
-    static constexpr auto mipsLabelDefinition = make_matcher<R"re(^\$[\w$.]+:)re">();
-    static constexpr auto dataDefn =
-    make_matcher<R"re(^\s*\.(string|asciz|ascii|[1248]?byte|short|x?word|long|quad|value|zero))re">();
-    static constexpr auto fileFind = make_matcher<R"re(^\s*\.file\s+(\d+)\s+"([^"]+)"(\s+"([^"]+)")?.*)re">();
-    // todo: stop using this static constexpr auto hasOpcodeRe = make_matcher<R"re(^\s*[A-Za-z])re">();
-    // todo: stop using this static constexpr auto instructionRe = make_matcher<R"re(^\s*[A-Za-z]+)re">();
-    static constexpr auto identifierFindRe = make_matcher<R"re([$.@A-Z_a-z][\dA-z]*)re">();
-    static constexpr auto hasNvccOpcodeRe = make_matcher<R"re(^\s*[@A-Za-z\|])re">();
-    static constexpr auto definesFunction =
-    make_matcher<R"re(^\s*\.(type.*,\s*[#%@]function|proc\s+[.A-Z_a-z][\w$.]*:.*)$)re">();
-    static constexpr auto definesGlobal = make_matcher<R"re(^\s*\.(?:globa?l|GLB|export)\s*([.A-Z_a-z][\w$.]*))re">();
-    static constexpr auto definesWeak = make_matcher<R"re(^\s*\.(?:weak|weakext)\s*([.A-Z_a-z][\w$.]*))re">();
-    // static constexpr auto indentedLabelDef = make_matcher<R"re(^\s*([$.A-Z_a-z][\w$.]*):)re">();
-    static constexpr auto assignmentDef = make_matcher<R"re(^\s*([$.A-Z_a-z][\w$.]+)\s*=)re">();
-    static constexpr auto directive = make_matcher<R"re(^\s*\..*$)re">();
-    static constexpr auto startAppBlock = make_matcher<R"re(\s*#APP.*)re">();
-    static constexpr auto endAppBlock = make_matcher<R"re(\s*#NO_APP.*)re">();
-    static constexpr auto startAsmNesting = make_matcher<R"re(\s*# Begin ASM.*)re">();
-    static constexpr auto endAsmNesting = make_matcher<R"re(\s*# End ASM.*)re">();
-    static constexpr auto cudaBeginDef = make_matcher<R"re(\.(entry|func)\s+(?:\([^)]*\)\s*)?([$.A-Z_a-z][\w$.]*)\($)re">();
-    static constexpr auto cudaEndDef = make_matcher<R"re(^\s*\)\s*$)re">();
-    // static constexpr auto asmOpcodeRe = make_matcher<R"re(^\s*([\da-f]+):\s*(([\da-f]{2} ?)+)\s*(.*))re">();
-    // static constexpr auto lineRe = make_matcher<R"re(^(/[^:]+):(\d+).*)re">();
-    // static constexpr auto labelRe = make_matcher<R"re(^([\da-f]+)\s+<([^>]+)>:$)re">();
-    // static constexpr auto destRe = make_matcher<R"re(\s([\da-f]+)\s+<([^+>]+)(\+0x[\da-f]+)?>$)re">();
-    // static constexpr auto commentRe = make_matcher<R"re([#;])re">();
-    static constexpr auto instOpcodeRe = make_matcher<R"re((\.inst\.?\w?)\s*(.*))re">();
-    // static constexpr auto blockComments = make_matcher<R"re(^[\t ]*/\*(\*(?!/)|[^*])*\*/\s*)re">();
-    static constexpr auto commentOnly = make_matcher<R"re(^\s*(((#|@|//).*)|(/\*.*\*/)|(;\s*)|(;[^;].*)|(;;.*\S.*))$)re">();
-    static constexpr auto commentOnlyNvcc = make_matcher<R"re(^\s*(((#|;|//).*)|(/\*.*\*/))$)re">();
-    static constexpr auto sourceTag = make_matcher<R"re(^\s*\.loc\s+(\d+)\s+(\d+).*)re">();
-    // static constexpr auto source6502Dbg = make_matcher<R"re(^\s*\.dbg\s+line,\s*"([^"]+)",\s*(\d+))re">();
-    // static constexpr auto sourceStab = make_matcher<R"re(^\s*\.stabn\s+(\d+),0,(\d+),.*)re">();
-    // static constexpr auto stdInLooking = make_matcher<R"re(<stdin>|^-$|example\.[^/]+$|<source>)re">();
-    static constexpr auto endBlock = make_matcher<R"re(\.(cfi_endproc|data|text|section))re">();
+    static constexpr auto labelDef = ctre::match<R"re(^(?:.proc\s+)?([\w$.@]+):)re">;
+    static constexpr auto labelFindNonMips = ctre::match<R"re([.A-Z_a-z][\w$.]*)re">;
+    static constexpr auto labelFindMips = ctre::match<R"re([$.A-Z_a-z][\w$.]*)re">;
+    static constexpr auto mipsLabelDefinition = ctre::match<R"re(^\$[\w$.]+:)re">;
+
+    static constexpr auto dataDefn = ctre::search<R"re(^\s*\.(string|asciz|ascii|[1248]?byte|short|x?word|long|quad|value|zero))re">;
+    static constexpr auto fileFind = ctre::match<R"re(^\s*\.file\s+(\d+)\s+"([^"]+)"(\s+"([^"]+)")?.*)re">;
+    // todo: stop using this static constexpr auto hasOpcodeRe = make_matcher<R"re(^\s*[A-Za-z])re">;
+    // todo: stop using this static constexpr auto instructionRe = make_matcher<R"re(^\s*[A-Za-z]+)re">;
+    static constexpr auto identifierFindRe = ctre::match<R"re([$.@A-Z_a-z][\dA-z]*)re">;
+    static constexpr auto hasNvccOpcodeRe = ctre::match<R"re(^\s*[@A-Za-z\|])re">;
+    static constexpr auto definesFunction = ctre::match<R"re(^\s*\.(type.*,\s*[#%@]function|proc\s+[.A-Z_a-z][\w$.]*:.*)$)re">;
+    static constexpr auto definesGlobal = ctre::match<R"re(^\s*\.(?:globa?l|GLB|export)\s*([.A-Z_a-z][\w$.]*))re">;
+    static constexpr auto definesWeak = ctre::match<R"re(^\s*\.(?:weak|weakext)\s*([.A-Z_a-z][\w$.]*))re">;
+    // static constexpr auto indentedLabelDef = make_matcher<R"re(^\s*([$.A-Z_a-z][\w$.]*):)re">;
+    static constexpr auto assignmentDef = ctre::match<R"re(^\s*([$.A-Z_a-z][\w$.]+)\s*=)re">;
+    static constexpr auto directive = ctre::match<R"re(^\s*\..*$)re">;
+    static constexpr auto startAppBlock = ctre::match<R"re(\s*#APP.*)re">;
+    static constexpr auto endAppBlock = ctre::match<R"re(\s*#NO_APP.*)re">;
+    static constexpr auto startAsmNesting = ctre::match<R"re(\s*# Begin ASM.*)re">;
+    static constexpr auto endAsmNesting = ctre::match<R"re(\s*# End ASM.*)re">;
+    static constexpr auto cudaBeginDef = ctre::match<R"re(\.(entry|func)\s+(?:\([^)]*\)\s*)?([$.A-Z_a-z][\w$.]*)\($)re">;
+    static constexpr auto cudaEndDef = ctre::match<R"re(^\s*\)\s*$)re">;
+    // static constexpr auto asmOpcodeRe = make_matcher<R"re(^\s*([\da-f]+):\s*(([\da-f]{2} ?)+)\s*(.*))re">;
+    // static constexpr auto lineRe = make_matcher<R"re(^(/[^:]+):(\d+).*)re">;
+    // static constexpr auto labelRe = make_matcher<R"re(^([\da-f]+)\s+<([^>]+)>:$)re">;
+    // static constexpr auto destRe = make_matcher<R"re(\s([\da-f]+)\s+<([^+>]+)(\+0x[\da-f]+)?>$)re">;
+    // static constexpr auto commentRe = make_matcher<R"re([#;])re">;
+    static constexpr auto instOpcodeRe = ctre::match<R"re((\.inst\.?\w?)\s*(.*))re">;
+    // static constexpr auto blockComments = make_matcher<R"re(^[\t ]*/\*(\*(?!/)|[^*])*\*/\s*)re">;
+    static constexpr auto commentOnly = ctre::match<R"re(^\s*(((#|@|//).*)|(/\*.*\*/)|(;\s*)|(;[^;].*)|(;;.*\S.*))$)re">;
+    static constexpr auto commentOnlyNvcc = ctre::match<R"re(^\s*(((#|;|//).*)|(/\*.*\*/))$)re">;
+    static constexpr auto sourceTag = ctre::match<R"re(^\s*\.loc\s+(\d+)\s+(\d+).*)re">;
+    // static constexpr auto source6502Dbg = make_matcher<R"re(^\s*\.dbg\s+line,\s*"([^"]+)",\s*(\d+))re">;
+    // static constexpr auto sourceStab = make_matcher<R"re(^\s*\.stabn\s+(\d+),0,(\d+),.*)re">;
+    // static constexpr auto stdInLooking = make_matcher<R"re(<stdin>|^-$|example\.[^/]+$|<source>)re">;
+    static constexpr auto endBlock = ctre::match<R"re(\.(cfi_endproc|data|text|section))re">;
 };
 
 
--- test/test_test.cpp
+++ test/test_test.cpp
@@ -9,11 +9,18 @@ TEST_CASE("Test some regexes directly", "[regex]") {
     CHECK(AsmParser::Regexes::labelDef(".mydirective:"));
     CHECK(!AsmParser::Regexes::labelDef("  mov eax, eax"));
 
-    // todo: this should work, but doesn't, why not?
-    // CHECK(AsmParser::Regexes::dataDefn(R"(  .string "Hello, world!\n")"));
+    CHECK(AsmParser::Regexes::dataDefn(R"(  .string "Hello, world!\n")"));
 
     CHECK(!AsmParser::Regexes::labelDef("  mov eax, eax"));
 
-    CHECK(AsmParser::Regexes::fileFind(R"(        .file 1 "/opt/compiler-explorer/gcc-10.2.0/include/c++/10.2.0/bits/char_traits.h")"));
-    CHECK(AsmParser::Regexes::sourceTag(R"(        .loc 1 351 7)"));
+    const auto fileMatch = AsmParser::Regexes::fileFind(R"(        .file 1 "/opt/compiler-explorer/gcc-10.2.0/include/c++/10.2.0/bits/char_traits.h")");
+    CHECK(fileMatch);
+    REQUIRE(fileMatch.get<1>().to_view() == "1");
+    REQUIRE(fileMatch.get<2>().to_view() == "/opt/compiler-explorer/gcc-10.2.0/include/c++/10.2.0/bits/char_traits.h");
+
+    const auto sourceMatch = AsmParser::Regexes::sourceTag(R"(        .loc 1 351 7)");
+    CHECK(sourceMatch);
+    REQUIRE(sourceMatch.get<1>().to_view() == "1");
+    REQUIRE(sourceMatch.get<2>().to_view() == "351");
+    
 }
