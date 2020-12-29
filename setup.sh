8a4a9d73ba09a866696f51597c4bd49834015367
partouf <partouf@gmail.com>
2020-12-29 21:33:05 +0000

    ..

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ sudo -H apt install -y python3 python3-pip python3-setuptools g++-9
 sudo -H pip3 install conan
 
 conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11
+conan profile update settings.compiler.libcxx=libstdc++11 default
