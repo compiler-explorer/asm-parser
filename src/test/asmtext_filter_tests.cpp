a0a820670a475bfc0c5af386e42c2b3e9380f55c
partouf <partouf@gmail.com>
2022-06-14 17:24:35 +0200

    fix tests

--- src/test/asmtext_filter_tests.cpp
+++ src/test/asmtext_filter_tests.cpp
@@ -7,6 +7,7 @@ auto directoryDisposer = ApprovalTests::Approvals::useApprovalsSubdirectory("../
 #include "../objdump/parser.hpp"
 #include "../types/filter.hpp"
 #include <iostream>
+#include <filesystem>
 
 TEST_CASE("HelloApprovals")
 {
@@ -21,9 +22,32 @@ TEST_CASE("filter_example")
     filter.library_functions = true;
     filter.unused_labels = true;
 
+    //std::cout << "cwd: " << std::filesystem::current_path() << "\n"; // we're in build/src/test
+
     AsmParser::ObjDumpParser parser(filter);
     std::fstream fs;
-    fs.open("../resources/example.asm", std::fstream::in);
+    fs.open("../../../resources/example.asm", std::fstream::in);
+    REQUIRE(fs.is_open() == true);
+
+    parser.fromStream(fs);
+
+    std::stringstream ss;
+    parser.outputText(ss);
+
+    ApprovalTests::Approvals::verify(ss.str());
+}
+
+TEST_CASE("gcc12_bin_fmt_O2_flto")
+{
+    AsmParser::Filter filter;
+    filter.binary = true;
+    filter.plt = true;
+    filter.library_functions = true;
+    filter.unused_labels = true;
+
+    AsmParser::ObjDumpParser parser(filter);
+    std::fstream fs;
+    fs.open("../../../resources/gcc12_bin_fmt_O2_flto.asm", std::fstream::in);
     REQUIRE(fs.is_open() == true);
 
     parser.fromStream(fs);
@@ -44,7 +68,7 @@ TEST_CASE("filter_example_intel")
 
     AsmParser::ObjDumpParser parser(filter);
     std::fstream fs;
-    fs.open("../resources/example_intel.asm", std::fstream::in);
+    fs.open("../../../resources/example_intel.asm", std::fstream::in);
     REQUIRE(fs.is_open() == true);
 
     parser.fromStream(fs);
