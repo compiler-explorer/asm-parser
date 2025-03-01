cca9b0d4adf04a5afbf95c27e594581701d7aa70
Ofek Shilon <ofekshilon@gmail.com>
2025-03-01 18:13:29 +0200

    Informative error message in case of missing locale

--- src/main.cpp
+++ src/main.cpp
@@ -70,8 +70,15 @@ AsmParserConfiguration getConfigurationFromCommandline(const int argc, const cha
 
 int main(int argc, const char **argv)
 {
-    std::locale loc("en_US.UTF-8");
-    std::locale::global(loc);
+    try {
+        std::locale loc("en_US.UTF-8");
+        std::locale::global(loc);
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Failed to set locale: " << e.what() << std::endl;
+        std::cerr << "Please make sure that the locale is installed on your system:" << std::endl;
+        std::cerr << "$ sudo apt-get install locales" << std::endl;
+        std::cerr << "$ locale-gen en_US.UTF-8" << std::endl;
+    }
 
     AsmParser::global_start_timer();
 
